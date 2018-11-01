ae8a70c0b4d7b867c941dd35207251eebb07cd7c
leandro bernardi <leandro.88.bernardi@gmail.com>
2018-11-01 20:52:27 -0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+class app extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <img alt="logo" className="App-logo" src={logo} />
+          <p>Edit and save to reload.</p>
           <a
             className="App-link"
             href="https://reactjs.org"
-            target="_blank"
             rel="noopener noreferrer"
+            target="_blank"
           >
             Learn React
           </a>
@@ -25,4 +21,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default app;
